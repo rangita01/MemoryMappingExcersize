059b92dd0b71b07fac92a1e02060834e091fde0a
Alex Wilson <wilsonalexanderjohn@byu.net>
2018-02-10 20:56:00 -0700

    Program now works. Removed debugging printf statements and error checking that didn't work because mmap triggers a core dump if used improperly.

--- mmap.c
+++ mmap.c
@@ -5,14 +5,13 @@
 #include <sys/stat.h>
 #include <stdio.h>
 #include <fcntl.h>
+#include <string.h>
 
 int main(int argc, char **argv)
 {
 	struct stat stat;
 
-	int fd = open("hello.txt", O_WRONLY);
-
-	printf("About to open file.\n");
+	int fd = open("hello.txt", O_RDWR);
 
 	if(fd > 0)
 	{
@@ -24,24 +23,11 @@ int main(int argc, char **argv)
 		return EXIT_FAILURE;
 	}
 
-	printf("File opened with descriptor %d\n", fd);
-
 	char *text;
 	
-	printf("Stats for mmap are as follows:\nsize:%ld\nfd:%d\n", stat.st_size, fd);
+	text = mmap(NULL, stat.st_size, PROT_WRITE | PROT_READ, MAP_SHARED, fd, 0);
 
-	text = mmap(NULL, stat.st_size, PROT_WRITE, MAP_SHARED, fd, 0);
-
-	if(text)
-	{
-		printf("About to attempt letter change...\n");
-		text[0] = 'J';
-	}
-	else
-	{
-		printf("mmap failed!\n");
-		return EXIT_FAILURE;
-	}
+	text[0] = 'J';
 
 	return EXIT_SUCCESS;
 }
